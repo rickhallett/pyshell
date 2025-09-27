79b793f21d03b93d2494f277dd5d213a7bb4fb99
rickhallett <rickhallett@icloud.com>
2025-09-27 21:56:56 +0100

    fix: get back to previous stage

--- app/main.py
+++ app/main.py
@@ -2,11 +2,23 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        user_input = input("$ ")
+        command, *args = user_input.split(" ")
 
-    # Wait for user input
-    input()
+        match command:
+            case "type":
+                builtins = ["type", "echo", "exit"]
+                if args[0] in builtins:
+                    return print(f"{args[0]} is a shell builtin")
+                else:
+                    return print(f"{args[0]}: not found")
+            case "echo":
+                print(" ".join(args))
+            case "exit":
+                sys.exit(int(args[0]) if args else 0)
+            case _:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
